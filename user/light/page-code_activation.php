3cba5ebb98ce0d4b3729fbdf15dfe2fa1ac8a1d4
Godsent Michael <gentlemotivator@gmail.com>
2023-02-08 16:54:19 +0100

    added code-activation page

---
+++ user/light/page-code_activation.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+<title>:: Network App :: Code Activation </title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<meta name="description" content="Lucid Bootstrap 4.1.1 Admin Template">
+<meta name="author" content="WrapTheme, design by: ThemeMakker.com">
+
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<!-- VENDOR CSS -->
+<link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="../assets/vendor/font-awesome/css/font-awesome.min.css">
+
+<!-- MAIN CSS -->
+<link rel="stylesheet" href="assets/css/main.css">
+<link rel="stylesheet" href="assets/css/color_skins.css">
+</head>
+
+<body class="none">
+	<!-- WRAPPER -->
+	<div id="wrapper">
+		<div class="vertical-align-wrap">
+			<div class="vertical-align-middle auth-main">
+				<div class="auth-box">
+                <div class="top">
+                        <img src="../assets/images/Naturcrystal logo png.png" alt="Naturcrystal" style="height: 100px;">
+                    </div>
+					<div class="card">
+                        <div class="header">
+                            <p class="lead">Get Activation Code</p>
+                        </div>
+                        <div class="body">
+                            <p>Please enter your activation code below to proceed to the login page.</p>
+                            <form class="form-auth-small" action="page-login.php">
+                                <div class="form-group">                                    
+                                    <input type="email" class="form-control" id="signup-password" placeholder="Enter code">
+                                </div>
+                                <button href="page-reset-password.php" type="submit" class="btn btn-primary btn-lg btn-block">Check code</button>
+                                <div class="bottom">
+                                    <span class="helper-text">Don't have a code? <a href="page-register.php">Register</a></span>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- END WRAPPER -->
+</body>
+</html>
+
